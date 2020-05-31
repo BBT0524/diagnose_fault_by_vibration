0e8c280876ae03e3756070dce4a0c7199e19b307
BBT0524 <2208191856@qq.com>
2020-05-31 14:55:41 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+#-*- coding:utf-8 -*-
+"""
+    创建于2020-05-31 14:40:32
+    作者：lbb
+    描述：对提取的特征进行分类器训练和模型的保存
+    其他：相关软件版本
+          matplotlib:  3.1.3  
+          numpy: 1.18.1  
+          scipy: 1.4.1 
+          pandas: 1.0.3
+          sklearn: 0.22.1 
+"""
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+
+from utils.augment import preprocess
+from utils.feature import extract_feature
+# -1- 载入数据
+path = r"./data/0HP"
+data_mark = "FE"
+len_data = 1024
+overlap_rate = 50 # 50%
+random_seed = 1 
+fs = 12000
+
+X, y = preprocess(path, 
+                    data_mark, 
+                    fs, 
+                    len_data/fs, 
+                    overlap_rate, 
+                    random_seed ) 
+
+# -2- 提取特征
+FX, Fy = extract_feature(X, y, fs)
+
+# -3- 数据集划分
+x_train, x_test, y_train, y_test = train_test_split(FX, 
+                                                    Fy, 
+                                                    test_size=0.10, 
+                                                    random_state=2)
+
+# -4- 模型训练和保存
+
+# K近邻
+knn = make_pipeline(StandardScaler(),  
+                     KNeighborsClassifier(3))
+knn.fit(x_train, y_train)
+
+score = knn.score(x_test, y_test) * 100
+print("KNN score is: %.3f"%score, "in test dataset")
+
+# 高斯分布的贝叶斯
+nbg = make_pipeline(StandardScaler(),  
+                    GaussianNB())
+nbg.fit(x_train, y_train)
+
+score = nbg.score(x_test, y_test) * 100
+print("GaussianNB score is: %.3f"%score, "in test dataset")
+# 随机森林
+rfc = make_pipeline(StandardScaler(),
+                    RandomForestClassifier(max_depth=6, random_state=0))
+rfc.fit(x_train, y_train)
+
+score = rfc.score(x_test, y_test) * 100
+print("RandomForest score is: %.3f"%score,  "in test dataset")
